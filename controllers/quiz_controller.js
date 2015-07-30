2dae3dd570cddf8f6a13fdb089dc29a6a43aa0dc
David Albaladejo <david.albaladejo@gmail.com>
2015-07-30 15:08:48 +0200

    Quiz 12: Validacion de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,11 +18,11 @@ exports.index = function(req, res) {
     var patron = '%'+req.query.search.replace(' ','%')+'%';
     models.Quiz.findAll({where: ["pregunta like ?",patron]}
       ).then(function(quizes) {
-        res.render('quizes/index.ejs', { quizes: quizes});
+        res.render('quizes/index.ejs', { quizes: quizes, errors: []});
       }).catch(function(error) {next(error)});
   } else {
     models.Quiz.findAll().then(function (quizes){
-        res.render('quizes/index.ejs', {quizes: quizes});
+        res.render('quizes/index.ejs', {quizes: quizes, errors: []});
     }).catch(function(error){next(error)});
   }
 };
@@ -32,32 +32,36 @@ exports.new = function(req,res) {
   var quiz = models.Quiz.build( // crea objeto quiz
     {pregunta: "Pregunta", respuesta: "Respuesta"}
   );
-  res.render('quizes/new', { quiz: quiz});
+  res.render('quizes/new', { quiz: quiz, errors: [] });
 };
 
 //GET /quizes/:quizId
 exports.show = function(req, res) {
   models.Quiz.find(req.params.quizId).then(function(quiz) {
-    res.render('quizes/show', { quiz: req.quiz});
-  })
-};
-//GET /quizes/answer
-exports.answer = function(req, res) {
-  models.Quiz.find(req.params.quizId).then(function(quiz) {
-    if (req.query.respuesta === req.quiz.respuesta) {
-      res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Correcto' });
-    } else {
-      res.render('quizes/answer', { quiz: req.quiz, respuesta: 'Incorrecto'});
-    }
+    res.render('quizes/show', { quiz: req.quiz, errors: [] });
   })
 };
 
+//GET /quizes/answer
+exports.answer = function (req, res) {
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta) {
+			resultado = 'Correcto';
+	}
+	res.render('quizes/answer',	{ quiz: req.quiz, respuesta: resultado, errors: [] });
+};
+
 // POST /quizes/create
-exports.create = function(req, res) {
+exports.create = function (req, res) {
   var quiz = models.Quiz.build(req.body.quiz);
-  // guarda en DB los campos pregunta y respuesta de quiz
-  quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-    res.redirect('/quizes');
-  }) // Redrección HTTP (URL relativa) lista de preguntas
+  quiz.validate().then(
+    function (err) {
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){res.redirect('/quizes');})
+      }
+    }
+  );
 };
 
